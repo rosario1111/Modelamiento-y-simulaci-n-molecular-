5ca2f1114575468e0fad321e95438058ee5a36bc
rosario1111 <ribanez2@uc.cl>
2020-10-30 15:28:10 -0300

    Add files via upload

---
+++ CW1/ancestro/align2D.py
@@ -0,0 +1,11 @@
+from modeller import *
+
+env = environ()
+aln = alignment(env)
+mdl = model(env, file='1uwq', model_segment=('FIRST:A','LAST:A'))
+aln.append_model(mdl, align_codes='1uwqA', atom_files='1uwq.pdb')
+aln.append(file='ancestro.fasta.txt', align_codes='Ancestro', alignment_format='FASTA')
+aln.align2d()
+aln.write(file='aligned.fasta', alignment_format='FASTA')
+aln.write(file='aligned.ali', alignment_format='PIR')
+aln.write(file='aligned.pap', alignment_format='PAP')
