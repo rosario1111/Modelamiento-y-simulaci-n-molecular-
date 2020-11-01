eac0d51da517ca4a7b28ffa4385762717c9cc409
rosario1111 <ribanez2@uc.cl>
2020-11-01 10:16:11 -0300

    Update align2D.py

--- CW1/ancestro/align2D.py
+++ CW1/ancestro/align2D.py
@@ -2,8 +2,8 @@ from modeller import *
 
 env = environ()
 aln = alignment(env)
-mdl = model(env, file='1uwqA', model_segment=('FIRST:A','LAST:A'))
-aln.append_model(mdl, align_codes='1uwqA', atom_files='1uwqA.pdb')
+mdl = model(env, file='1uwq_limpio', model_segment=('FIRST:A','LAST:A'))
+aln.append_model(mdl, align_codes='1uwqA', atom_files='1uwq_limpio.pdb')
 aln.append(file='ancestro.fasta.txt', align_codes='Ancestro', alignment_format='FASTA')
 aln.align2d()
 aln.write(file='aligned.fasta', alignment_format='FASTA')
